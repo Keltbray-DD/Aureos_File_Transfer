a7e848adf8f53d595f3158644d1cdd9eb131f139
Keltbray-DD <digi@keltbray.com>
2025-03-31 10:33:54 +0100

    v0.1.2 added login script

--- js/variables.js
+++ js/variables.js
@@ -1,20 +1,31 @@
+// App variables
 const appName = "Aureos File Transfer Portal";
-const appVersion = "v0.1.1 ALPHA";
+const appVersion = "v0.1.2 ALPHA";
+const topFolder = "urn:adsk.wipemea:fs.folder:co.kZUqIqGpTduHhwHLpGBE8A"
+const projectId = "73a986af-0afd-4562-b630-bb614992d96b"
 
-let userEmail
+// Create Package variables
 let globalFiles
 let accessToken
 let packageFolderId
 let packageId
 let uploadfile
-let urlParams
-
-let errorElement
-
-// Array to keep track of selected files
 let selectedFiles = [];
+
 const bucketKey = "wip.dm.emea.2"
 
-const topFolder = "urn:adsk.wipemea:fs.folder:co.kZUqIqGpTduHhwHLpGBE8A"
-const projectId = "73a986af-0afd-4562-b630-bb614992d96b"
+// View Package variables
+let urlParams
+let errorElement
+
+// Login Variables
+let userDetails
+let userEmail
+let userAccessToken
+let userRefreshToken
+let toolURL = ""
+
+
+
+
 
